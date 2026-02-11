2f2207002a06b4c9b798dec4f6d8038d33801442
Ujjawal Kumar <ujjawal1422@gmail.com>
2026-02-11 11:16:20 +0530

    third commit

--- src/main/java/com/jspider/spring_core_technology/controller/CustomerDriver.java
+++ src/main/java/com/jspider/spring_core_technology/controller/CustomerDriver.java
@@ -4,8 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.jspider.spring_core_technology.config.MySpringConfig;
-//import com.jspider.spring_core_technology.dto.Customer;
-import com.jspider.spring_core_technology.controller.Customer;
+import com.jspider.spring_core_technology.dto.Customer;
+//import com.jspider.spring_core_technology.controller.Customer;
 
 public class CustomerDriver {
 	
@@ -15,12 +15,12 @@ public class CustomerDriver {
 		
 		System.out.println("Container created");
 		
-		Customer customer=(Customer) applicationContext.getBean("customer");
+		Customer customer=(Customer) applicationContext.getBean("dtocustomer");
 		
 		System.out.println(customer.getId());
-//		System.out.println(customer.getName());
-//		System.out.println(customer.getEmail());
-//		System.out.println(customer.getPhone());
+		System.out.println(customer.getName());
+		System.out.println(customer.getEmail());
+		System.out.println(customer.getPhone());
 	}
 
 }
